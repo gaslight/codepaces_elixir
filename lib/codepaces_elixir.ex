3dbbdaeb8e30324f37e5a0c683dc983a1f85a8f3
Chris Nelson <chris@gaslight.co>
2021-11-15 18:02:37 +0000

    initial commit

---
+++ lib/codepaces_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CodepacesElixir do
+  @moduledoc """
+  Documentation for `CodepacesElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodepacesElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/codepaces_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CodepacesElixirTest do
+  use ExUnit.Case
+  doctest CodepacesElixir
+
+  test "greets the world" do
+    assert CodepacesElixir.hello() == :world
+  end
+end
